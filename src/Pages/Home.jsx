c0f6857c4d1aa656beecc1c92d4dd6da7ea016a6
Claudette <cobbahclaudette@gmail.com>
2025-06-20 23:57:47 +0000

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -25,7 +25,7 @@ const MainTitle = memo(() => (
       <span className="relative inline-block">
         <span className="absolute -inset-2 bg-gradient-to-r from-[#6366f1] to-[#a855f7] blur-2xl opacity-20"></span>
         <span className="relative bg-gradient-to-r from-white via-blue-100 to-purple-200 bg-clip-text text-transparent">
-          Frontend
+          Backend
         </span>
       </span>
       <br />
@@ -80,9 +80,9 @@ const PAUSE_DURATION = 2000;
 const WORDS = ["Network & Telecom Student", "Tech Enthusiast"];
 const TECH_STACK = ["React", "Javascript", "Node.js", "Tailwind"];
 const SOCIAL_LINKS = [
-  { icon: Github, link: "https://github.com/EkiZR" },
-  { icon: Linkedin, link: "https://www.linkedin.com/in/ekizr/" },
-  { icon: Instagram, link: "https://www.instagram.com/ekizr._/?hl=id" }
+  { icon: Github, link: "https://github.com/claudette05" },
+  { icon: Linkedin, link: "https://www.linkedin.com/in/claudette-cobbah-4969942b5?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+  { icon: Instagram, link: "https://www.instagram.com/_claudette.ee/" }
 ];
 
 const Home = () => {
@@ -183,7 +183,8 @@ const Home = () => {
                 <p className="text-base md:text-lg text-gray-400 max-w-xl leading-relaxed font-light"
                   data-aos="fade-up"
                   data-aos-delay="1000">
-                  Menciptakan Website Yang Inovatif, Fungsional, dan User-Friendly untuk Solusi Digital.
+                Crafting digital experiences that are innovative, functional, and empowering for real-world users.
+
                 </p>
 
                 {/* Tech Stack */}
